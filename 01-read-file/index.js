2bfc9a5619a3fda8139401cb786860e954bc6c62
kivlaros <kivlaros@gmail.com>
2022-11-01 18:23:10 +0300

    feat: add 01-read-file task

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = __dirname;
+
+let readableStream = fs.createReadStream(`${path}/text.txt`, 'utf8')
+readableStream.on('data', function(chunk) { 
+	console.log(chunk);
+});
+
