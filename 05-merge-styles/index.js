8677c0e13966c19f793d5027354717614ba49520
kivlaros <kivlaros@gmail.com>
2022-11-03 10:50:21 +0300

    feat: add 05-merge-styles solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path')
+
+const styleDir = `${__dirname}/styles`
+const distDir = `${__dirname}/project-dist/bundle.css`
+
+async function mergeStyles(){
+  await fs.promises.writeFile(distDir,'')
+  const files = await fs.promises.readdir(styleDir,{withFileTypes: true});
+  for (const file of files){
+    if(path.extname(file.name)=='.css'){
+      //console.log(file)
+      let data = await fs.promises.readFile(`${styleDir}/${file.name}`,'utf-8')
+      await fs.promises.appendFile(distDir,`${data}\n`)
+    }
+  }
+}
+
+mergeStyles()
